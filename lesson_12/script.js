665652354cc7c46fca691f0d72ed934336763e5b
Artsion Zavaley <penin13579@gmail.com>
2019-09-24 17:47:41 +0300

    12, 13 lessons done

---
+++ lesson_12/script.js
@@ -0,0 +1,22 @@
+/* Tasks */
+
+//1 - Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+
+// function equalNumbers(num1, num2) {
+//   return num1 == num2;
+// }
+// document.write(equalNumbers(1, 2));
+
+//2 - Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+
+// function numbersFunction(num1, num2) {
+//   return num1 + num2 > 10;
+// }
+// document.write(numbersFunction(11, 2));
+
+//3 - Сделайте функцию, которая параметром принимает число и проверяет - отрицательное оно или нет. Если отрицательное - пусть функция вернет true, а если нет - false.
+
+// function positiveNumberCheck(num1) {
+//   return num1 < 0;
+// }
+// document.write(positiveNumberCheck(-11));
